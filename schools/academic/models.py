e38e218d52faa820f1179152443f6f20ac2eb58b
joanayala <jflaco85@gmail.com>
2021-09-29 07:53:16 -0500

    Create schools project

---
+++ schools/academic/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class people(models.Model) :
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    id_type = models.IntegerField()
+    number_id = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, default = "")
